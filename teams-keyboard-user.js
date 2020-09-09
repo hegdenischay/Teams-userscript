7033c77efa298400c59d0681eb7284ce76922889
Nischay <hegdenischay@gmail.com>
2020-09-09 16:20:06 +0530

    Added Mute and Hangup keybinds

--- teams-keyboard-user.js
+++ teams-keyboard-user.js
@@ -2,7 +2,7 @@
 // @name        Teams keyboard Shortcuts
 // @namespace   Violentmonkey Scripts
 // @include     https://teams.microsoft.com/*
-// @inject      auto
+// @inject-into page
 // @version     0.1
 // @author      Nischay
 // @description 9/9/2020, 3:38:48 PM
@@ -13,7 +13,7 @@ function sleep (time) {
 }
 
 document.addEventListener('keyup', (event) => {
-    if (event.key == 'f') {
+    if (event.key == 'f' || event.key == 'F') {
         document.getElementsByTagName('button')['callingButtons-showMoreBtn'].click();
         //I use sleep here because teams otherwise throws a tantrum
         sleep(500).then(() => {
@@ -21,3 +21,12 @@ document.addEventListener('keyup', (event) => {
     });
     }});
 
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'm' || event.key == 'M') {
+        document.getElementsByTagName('button')['microphone-button'].click();
+    }});
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'h' || event.key == 'H') {
+        document.getElementsByTagName('button')['hangup-button'].click();
+    }});
