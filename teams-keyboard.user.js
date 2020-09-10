e9173b33af32a98a3243ccd6c34457a02283d070
Nischay <hegdenischay@gmail.com>
2020-09-10 14:45:41 +0530

    Incredibly hacky code to make it only work in the main call

--- teams-keyboard.user.js
+++ teams-keyboard.user.js
@@ -12,26 +12,36 @@ function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
+var work = 0;
+
 document.addEventListener('keyup', (event) => {
-    if (event.key == 'f' || event.key == 'F') {
+  if (event.key == 'f' || event.key == 'F') {
+    work = document.getElementsByClassName("call-screen-main")['length'];
+    if(work ===1){
         document.getElementsByTagName('button')['callingButtons-showMoreBtn'].click();
         //I use sleep here because teams otherwise throws a tantrum
         sleep(500).then(() => {
         document.getElementsByTagName('button')['full-screen-button'].click();
     });
-    }});
+    }}});
 
 document.addEventListener('keyup', (event) => {
     if (event.key == 'm' || event.key == 'M') {
+    work = document.getElementsByClassName("call-screen-main")['length'];
+      if(work === 1){
         document.getElementsByTagName('button')['microphone-button'].click();
-    }});
+    }}});
 
 document.addEventListener('keyup', (event) => {
     if (event.key == 'h' || event.key == 'H') {
+    work = document.getElementsByClassName("call-screen-main")['length'];
+      if(work === 1){
         document.getElementsByTagName('button')['hangup-button'].click();
-    }});
+    }}});
 
 document.addEventListener('keyup', (event) => {
     if (event.key == 'v' || event.key == 'V') {
+    work = document.getElementsByClassName("call-screen-main")['length'];
+      if(work === 1){
         document.getElementsByTagName('button')['video-button'].click();
-    }});
+    }}});
