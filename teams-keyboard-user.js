d3096af8c0162e99afbbc1a39e18a1ab289ed382
Nischay <hegdenischay@gmail.com>
2021-04-21 09:30:56 +0530

    Made the shortcuts more in line with shortcuts given by Teams

--- teams-keyboard-user.js
+++ teams-keyboard-user.js
@@ -5,7 +5,7 @@
 // @inject-into page
 // @version     0.1
 // @author      Nischay
-// @description 9/9/2020, 3:38:48 PM
+// @description 21/04/2021, 09:29:48 PM
 // ==/UserScript==
 
 function sleep (time) {
@@ -14,8 +14,9 @@ function sleep (time) {
 
 var work = 0;
 
+
 document.addEventListener('keyup', (event) => {
-  if (event.key == 'f' || event.key == 'F') {
+  if ((event.key == 'f' || event.key == 'F') && (event.shiftKey === true && event.ctrlKey === true)) {
     work = document.getElementsByClassName("call-screen-main")['length'];
     if(work ===1){
         document.getElementsByTagName('button')['callingButtons-showMoreBtn'].click();
@@ -26,22 +27,23 @@ document.addEventListener('keyup', (event) => {
     }}});
 
 document.addEventListener('keyup', (event) => {
-    if (event.key == 'm' || event.key == 'M') {
+    if ((event.key == 'm' || event.key == 'M') && (event.shiftKey === true && event.ctrlKey === true)) {
     work = document.getElementsByClassName("call-screen-main")['length'];
       if(work === 1){
         document.getElementsByTagName('button')['microphone-button'].click();
     }}});
 
 document.addEventListener('keyup', (event) => {
-    if (event.key == 'h' || event.key == 'H') {
+    if ((event.key == 'h' || event.key == 'H') && (event.shiftKey === true && event.ctrlKey === true)) {
     work = document.getElementsByClassName("call-screen-main")['length'];
       if(work === 1){
         document.getElementsByTagName('button')['hangup-button'].click();
     }}});
 
 document.addEventListener('keyup', (event) => {
-    if (event.key == 'v' || event.key == 'V') {
+    if ((event.key == 'v' || event.key == 'V') && (event.shiftKey === true && event.ctrlKey === true)) {
     work = document.getElementsByClassName("call-screen-main")['length'];
       if(work === 1){
         document.getElementsByTagName('button')['video-button'].click();
     }}});
+
